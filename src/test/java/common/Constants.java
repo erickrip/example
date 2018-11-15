e4b59c2f7de9ff5cf63db5d88139ff387950dd19
gsanchez <gsanchez@bluecedar.com>
2018-11-15 14:17:24 -0600

    Added reports

--- src/test/java/common/Constants.java
+++ src/test/java/common/Constants.java
@@ -1,5 +1,8 @@
 package common;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Constants {
 
     public static String BASE_URL = "https://pokeapi.co/api/v2/";
@@ -7,5 +10,8 @@ public class Constants {
     public static String LOCATION_SERVICE = "location/";
     public static String BERRI_SERVICE = "berry/";
 
+    public static final String HTML_REPORT = System.getProperty("user.dir") + "/results/api_report" +
+            new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime()).toString() + ".html";
+    public static final String EXTENT_CONFIG = System.getProperty("user.dir") + "/extent-config.xml";
 
 }
--- src/test/java/test/ExamplesTest.java
+++ src/test/java/test/ExamplesTest.java
@@ -1,18 +1,39 @@
 package test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.LogStatus;
 import common.Constants;
 import common.Utils;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import common.ExtentProperties;
+
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
 
 public class ExamplesTest {
     private RequestSpecification reqSpec;
     private Response respValid;
+    protected static ExtentProperties exProp;
+    private static String extentReportFile;
+
+    @BeforeSuite
+    public void beforeSuite() {
+        extentReportFile = Constants.HTML_REPORT;
+        exProp = new ExtentProperties();
+        // Create object of extent report and specify the report file path.
+        exProp.setExtent(new ExtentReports(extentReportFile, true));
+
+        exProp.getExtent().loadConfig(new File(Constants.EXTENT_CONFIG));
+
+    }
+
+
     @BeforeTest
     public void setUp(){
         reqSpec = Utils.getRequestSpecification();
@@ -20,6 +41,7 @@ public class ExamplesTest {
 
     @Test(priority = 1)
     public void searchMonsterByID(){
+        startTestCase("Searh a monster by ID", "Search the pokemon with ID 1");
         respValid = given()
                 .spec(reqSpec)
                 .when()
@@ -28,10 +50,13 @@ public class ExamplesTest {
         respValid.then().statusCode(HttpStatus.SC_OK);
 
         Assert.assertEquals(respValid.path("name"), "bulbasaur");
+        logInfo("Pokemon name: " + respValid.path("name"));
+        logPassStep("Status code: " + HttpStatus.SC_OK);
     }
 
     @Test(priority = 2)
     public void searchMonsterByName(){
+        startTestCase("Search a monster by name", "Recover a pokemon with the name Charizard");
         respValid = given()
                 .spec(reqSpec)
                 .when()
@@ -41,10 +66,15 @@ public class ExamplesTest {
 
         Assert.assertEquals(respValid.path("name"), "charizard");
         Assert.assertEquals(respValid.path("id"), 6);
+
+        logInfo("Pokemon name: " + respValid.path("name"));
+        logInfo("Pokemon ID: " + respValid.path("id"));
+        logPassStep("Test Passed");
     }
 
     @Test(priority = 2)
     public void searchMonsterByNameNotFound(){
+        startTestCase("Search a monster that not exist", "Generate a GET 404 (resource not found)");
         respValid = given()
                 .spec(reqSpec)
                 .when()
@@ -52,10 +82,12 @@ public class ExamplesTest {
 
         respValid.then().log().all().statusCode(HttpStatus.SC_NOT_FOUND);
 
+        logPassStep(respValid.prettyPrint());
     }
 
     @Test(priority = 3)
     public void searchRegion(){
+        startTestCase("Search a city region", "Search a city");
         respValid = given()
                 .spec(reqSpec)
                 .when()
@@ -65,8 +97,63 @@ public class ExamplesTest {
         Assert.assertEquals(respValid.path("name"), "canalave-city");
         Assert.assertEquals(respValid.path("region.name"), "sinnoh");
 
+        logInfo("City name: " + respValid.path("name"));
+        logInfo("Region name: " + respValid.path("region.name"));
+        logPassStep("Test passed");
+
     }
 
+
+    @AfterMethod
+    public void getResult(ITestResult result){
+        switch (result.getStatus()){
+            case ITestResult.FAILURE:
+                logTestFail(result);
+                logFailStep(result.getThrowable().toString());
+                break;
+            case ITestResult.SUCCESS:
+                logTestPass(result);
+                break;
+            case ITestResult.SKIP:
+                logTestSkip(result);
+                break;
+        }
+    }
+
+    @AfterSuite
+    public void tearDown(){
+        exProp.getExtentTest().log(LogStatus.INFO, "Execution Completed!");
+        // close report.
+        exProp.getExtent().endTest(exProp.getExtentTest());
+        // writing everything to document.
+        exProp.getExtent().flush();
+    }
+
+    public void startTestCase(String testName, String desc){
+        exProp.setExtentTest(exProp.getExtent().startTest(testName, desc));
+    }
+
+    private void logTestPass(ITestResult result){
+        exProp.getExtentTest().log(LogStatus.PASS, result.getName() + " Passed");
+    }
+
+    private void logTestFail(ITestResult result){
+        exProp.getExtentTest().log(LogStatus.FAIL, result.getName() + " Failed" );
+    }
+
+    private void logTestSkip(ITestResult result){
+        exProp.getExtentTest().log(LogStatus.SKIP, result.getName() + " Skipped");
+    }
+
+    public void logInfo(String details){
+        exProp.getExtentTest().log(LogStatus.INFO, details );
+    }
+
+    public void logPassStep(String details){
+        exProp.getExtentTest().log(LogStatus.PASS, details);
+    }
+
+    public void logFailStep(String details) { exProp.getExtentTest().log(LogStatus.FAIL, details); }
 
 
 }
